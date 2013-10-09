bf24e86654c772479d8d5f2d9d08a18b39f1aad3
Narendran Kumaragurunathan <narenkn@gmail.com>
2013-10-09 13:56:25 +0800

    8/Oct Off

--- units/menu.c
+++ units/menu.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <p89v51rd2.h>
 
 #include "menu.h"
 
@@ -9,7 +9,7 @@
 #define MENU_FUNC(A)
 #define ARG1(A, B)
 #define ARG2(A, B)
-char *menu_names[] = {
+char *menu_names = {
   MENU_ITEMS
 };
 #undef  ARG2
@@ -27,7 +27,7 @@ typedef void menu_func_t(void);
 #define MENU_FUNC(A) A
 #define ARG1(A, B)
 #define ARG2(A, B)
-menu_func_t menu_names[] = {
+menu_func_t menu_handlers[] = {
   MENU_ITEMS
 };
 #undef  ARG2
@@ -53,14 +53,31 @@ uint8_t menu_args[] = {
 #undef  ROW_JOIN
 #undef  COL_JOIN
 
+#define ROW_JOIN ,
+#define COL_JOIN
+#define MENU_NAME(A)
+#define MENU_FUNC(A)
+#define ARG1(A, B) A,
+#define ARG2(A, B) A
+uint8_t menu_prompts[] = {
+  MENU_ITEMS
+};
+#undef  ARG2
+#undef  ARG1
+#undef  MENU_FUNC
+#undef  MENU_NAME
+#undef  ROW_JOIN
+#undef  COL_JOIN
+
 void
 menu_main(void)
 {
-}
-
-int
-main(void)
-{
-  menu_main();
-  return 0;
+  uint8_t select;
+  do {
+    printf(menu_names[select]);
+    while (0!=KbdHit) {
+      sleep(1);
+    }
+    if (KbdHit < )
+  } while(1);
 }
