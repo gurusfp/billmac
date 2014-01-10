07820684220c326367f08e73550a5e1f11c28498
Narendran Kumaragurunathan <narenkn@gmail.com>
2014-01-10 21:31:04 +0530

    attempt to compile in sdcc

--- tests/test_kbd.c
+++ tests/test_kbd.c
@@ -35,15 +35,13 @@ main(void)
   LCD_WR_LINE(1, 0, "Shall displayed!");
   KBD_RESET_KEY;
   while (1) {
-    if (KBD_HIT) {
-      KBD_GET_KEY;
-      LCD_POS(1, 0);
-      LCD_WR("Scan Code : ");
-      LCD_PUT_UINT8X(KbdData);
-      KBD_RESET_KEY;
-    }
+    KBD_GET_KEY;
+    LCD_POS(1, 0);
+    LCD_WR("Scan Code : ");
+    LCD_PUT_UINT8X(KbdData);
     for (ui1=0; ui1<0xFF; ui1++)
       {}
+    KBD_RESET_KEY;
   }
 
   return 0;
